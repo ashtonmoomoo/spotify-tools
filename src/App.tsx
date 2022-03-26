0643d7efe241cad3fead2e3afb8813a1dd276528
Ashton Moore <ashton.chris.moore@gmail.com>
2022-03-26 14:57:27 +1300

    export functionality actually works but needs a refactor lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,49 @@
-import { Buffer } from "buffer";
 import { useEffect, useState } from "react";
-import Prompt from "./components/Prompt";
-
-function objectToQueryString(obj: any) {
-  return Object.entries(obj)
-    .map(([key, value]) => `${key}=${value}`)
-    .join("&");
-}
-
-function getAuthUrl() {
-  const spotifyAuthEndpoint = "https://accounts.spotify.com/authorize?";
-  const clientId = process.env.REACT_APP_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_REDIRECT_URI;
-
-  if (!clientId || !redirectUri) {
-    throw new Error("Missing config");
-  }
-
-  const params = {
-    client_id: clientId,
-    response_type: "code",
-    redirect_uri: redirectUri,
-    scope: "user-library-modify user-library-read",
-  };
-
-  const queryString = objectToQueryString(params);
-
-  return spotifyAuthEndpoint + queryString;
-}
-
-async function getAuthToken(authCode: string) {
-  const spotifyTokenEndpoint = "https://accounts.spotify.com/api/token";
-  const redirectUri = process.env.REACT_APP_REDIRECT_URI;
-
-  if (!redirectUri) {
-    throw new Error("Missing config");
-  }
-
-  const params = {
-    code: authCode,
-    redirect_uri: redirectUri,
-    grant_type: "authorization_code",
-  };
-
-  function getEncodedAuthString() {
-    const clientId = process.env.REACT_APP_CLIENT_ID;
-    const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
-    const encoded = Buffer.from(clientId + ":" + clientSecret).toString(
-      "base64"
-    );
-
-    return "Basic " + encoded;
-  }
-
-  const response = await fetch(spotifyTokenEndpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      Authorization: getEncodedAuthString(),
-    },
-    body: objectToQueryString(params),
-  });
-
-  return response;
-}
-
-function Login() {
-  return <a href={getAuthUrl()}>Log in with Spotify</a>;
-}
+import { Link } from "react-router-dom";
+import getTokenFromCookie from "./utils/getTokenFromCookie";
 
 function App() {
-  const url = new URL(window.location.href);
-  const authCode = url.searchParams.get("code");
-  let [authToken, setAuthToken] = useState<string>("");
+  const [token, setToken] = useState<string | undefined>(undefined);
 
   useEffect(() => {
-    if (!authCode) return;
-
-    const fetchAuthToken = async () => {
-      const response = await getAuthToken(authCode);
-      if (!response.ok) throw new Error("Failed to get access token :(");
-
-      const body = await response.json();
-      console.log(body);
-
-      setAuthToken(body["access_token"]);
-    };
-
-    fetchAuthToken();
-  }, [authCode]);
+    const t = getTokenFromCookie();
+    setToken(t);
+  }, []);
 
   return (
     <>
-      <h1>Spotify deduplicater</h1>
-      {authToken ? <Prompt token={authToken} /> : <Login />}
+      <h1>Spotify Tools</h1>
+      {!token ? <Link to="/login">Login</Link> : null}
+      <ul>
+        <li>
+          <Link to="/export-import">
+            [WIP] Export / Import your liked songs and playlists
+          </Link>
+        </li>
+        <li>
+          <Link to="duplicates">
+            [WIP] Remove duplicate songs and albums from your liked songs and
+            playlists
+          </Link>
+        </li>
+        <li>
+          <Link to="like-missing-songs">
+            [WIP] Like all the songs from albums you have liked
+          </Link>
+        </li>
+        <li>
+          <Link to="remove-liked-songs">
+            [WIP] Experimental: Remove liked songs from albums you <i>don't</i>{" "}
+            like
+          </Link>
+        </li>
+        <li>
+          <Link to="lastfm-tools">
+            [WIP] Create Spotify playlists based on your Last.fm listening
+            history
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Export from "./components/Export";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="login" element={<Login />} />
+        <Route path="export-import" element={<Export />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
