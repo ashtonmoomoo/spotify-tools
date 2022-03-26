015d8d300d8756b26a8a8940434f65b47447f5c2
Ashton Moore <ashton.chris.moore@gmail.com>
2022-03-26 16:01:33 +1300

    color haha

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import getTokenFromCookie from "./utils/getTokenFromCookie";
+import "./styles/global.css";
 
 function App() {
   const [token, setToken] = useState<string | undefined>(undefined);
