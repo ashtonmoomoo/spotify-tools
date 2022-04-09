5c1559854315e5f04b0e9646cc7fb48f935eec73
Ashton Moore <ashton.chris.moore@gmail.com>
2022-04-09 18:15:50 +1200

    The O of SOLID

--- src/components/FeatureList.tsx
+++ src/components/FeatureList.tsx
@@ -1,35 +1,36 @@
 import { Link } from "react-router-dom";
 
+const FEATURES = [
+  {
+    url: "/export-import",
+    text: "[WIP] Export / Import your liked songs and playlists",
+  },
+  {
+    url: "/duplicates",
+    text: "[WIP] Remove duplicate songs and albums from your liked songs and playlists",
+  },
+  {
+    url: "/like-missing-songs",
+    text: "[WIP] Like all the songs from albums you have liked",
+  },
+  {
+    url: "/remove-liked-songs",
+    text: "[WIP] Experimental: Remove liked songs from albums you <i>don't</i>like",
+  },
+  {
+    url: "/lastfm-tools",
+    text: "[WIP] Create Spotify playlists based on your Last.fm listening history",
+  },
+];
+
 export function FeatureList() {
   return (
     <ul>
-      <li>
-        <Link to="/export-import">
-          [WIP] Export / Import your liked songs and playlists
-        </Link>
-      </li>
-      <li>
-        <Link to="duplicates">
-          [WIP] Remove duplicate songs and albums from your liked songs and
-          playlists
-        </Link>
-      </li>
-      <li>
-        <Link to="like-missing-songs">
-          [WIP] Like all the songs from albums you have liked
-        </Link>
-      </li>
-      <li>
-        <Link to="remove-liked-songs">
-          [WIP] Experimental: Remove liked songs from albums you <i>don't</i>{" "}
-          like
-        </Link>
-      </li>
-      <li>
-        <Link to="lastfm-tools">
-          [WIP] Create Spotify playlists based on your Last.fm listening history
-        </Link>
-      </li>
+      {FEATURES.map((feature) => (
+        <li>
+          <Link to={feature.url}>{feature.text}</Link>
+        </li>
+      ))}
     </ul>
   );
 }
